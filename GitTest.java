36c1c5bac3763d9d5ec0fa900b6c295cc4b9281d
NathenKorsten <53427397+NathenKorsten@users.noreply.github.com>
2019-08-21 15:41:15 +0200

    Delete GitTest.java

--- GitTest.java
+++
@@ -1,9 +0,0 @@
-Public class GitTest
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World !");
-		System.out.println("This is a test.");
-		System.out.println("This is the 2nd test.");
-	}
-}
